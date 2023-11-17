c1e973f810c0a1b15cc55b64c24997ad311e018f
Mercier Laurent <mercier_laurent@live.fr>
2023-11-17 14:20:57 +0100

    week in past in weekSelector are not selectable

--- build/frontend.asset.php
+++ build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '2968343e88aaedbff292');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'be592f499eb569ce249b');
